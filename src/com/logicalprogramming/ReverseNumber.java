856a897cf379102172e44acea69e20badd5f36ec
Shaikh Afzal <xfzxlshxikh@gmail.com>
2021-09-15 11:56:19 +0530

    Solved: Reverse Number Problem.

--- src/com/logicalprogramming/ReverseNumber.java
+++ src/com/logicalprogramming/ReverseNumber.java
@@ -1,10 +1,29 @@
 package com.logicalprogramming;
 
+import java.util.Scanner;
+
 public class ReverseNumber {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Enter A Number To Get Reverse: ");
+		int input = scanner.nextInt();
+
+		ReverseNumber rObject = new ReverseNumber();
+		rObject.reverseNumber(input);
+		scanner.close();
 	}
 
+	void reverseNumber(int number) {
+
+		int reminder = 0, reverse = 0;
+
+		while (number != 0) {
+			reminder = number % 10;
+			reverse = reverse * 10 + reminder;
+			number = number / 10;
+		}
+		System.out.println("Reverse Number: " + reverse);
+	}
 }
