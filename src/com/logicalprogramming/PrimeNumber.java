95eff14f7c189b659ae0c626c93d83eaabe92f08
Shaikh Afzal <xfzxlshxikh@gmail.com>
2021-09-15 11:39:16 +0530

    Solved: Prime Number Problem.

--- src/com/logicalprogramming/PrimeNumber.java
+++ src/com/logicalprogramming/PrimeNumber.java
@@ -1,10 +1,32 @@
 package com.logicalprogramming;
 
+import java.util.Scanner;
+
 public class PrimeNumber {
-
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Enter Number To Check Prime Or Not: ");
+		int input = scanner.nextInt();
+		
+		PrimeNumber pObj = new PrimeNumber();
+		pObj.primeNumber(input);
+		scanner.close();
 	}
-
+	
+	void primeNumber(int number) {
+		int temp = 0;
+		for(int i = 2; i <= number - 1; i++) {
+			
+			if(number % i == 0) {
+				temp += 1;
+			}
+		}
+		
+		if(temp > 0) {
+			System.out.println("Not A Prime Number");
+		} else {
+			System.out.println("Prime Number");
+		}
+	}
 }
