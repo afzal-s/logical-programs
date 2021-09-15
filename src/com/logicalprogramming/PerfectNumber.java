b956e4713de9112b879f3fa2d0d456d347ed7cda
Shaikh Afzal <xfzxlshxikh@gmail.com>
2021-09-15 11:23:51 +0530

    Solved: Perfect Number Problem.

--- src/com/logicalprogramming/PerfectNumber.java
+++ src/com/logicalprogramming/PerfectNumber.java
@@ -1,10 +1,31 @@
 package com.logicalprogramming;
 
+import java.util.Scanner;
+
 public class PerfectNumber {
-
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Enter Number To Check Given Number Is Perfect Number Or Not: ");
+		int input = scanner.nextInt();
+		scanner.close();
+		
+		perfectNumber(input);
 	}
-
-}
+	
+	static void perfectNumber(int number) {
+		int sum = 0;
+		
+		for (int i = 1; i < number; i++) {
+			if (number % i == 0) {
+				sum = sum + i;
+			}
+		}
+		
+		if (sum == number) {
+			System.out.println("Perfect Number");
+		} else {
+			System.out.println("Not Perfect Number");
+		}
+	}
+}   
