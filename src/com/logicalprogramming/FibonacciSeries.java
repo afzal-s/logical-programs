51bc61d62ea7cd23d998e27e93839368597da8ae
Shaikh Afzal <xfzxlshxikh@gmail.com>
2021-09-15 10:30:49 +0530

    Solved: Fibonacci Series Problem.

--- src/com/logicalprogramming/FibonacciSeries.java
+++ src/com/logicalprogramming/FibonacciSeries.java
@@ -1,10 +1,30 @@
 package com.logicalprogramming;
 
+import java.util.Scanner;
+
 public class FibonacciSeries {
-
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Enter Number To Generate Fibonacci Series: ");
+		int input = scanner.nextInt();
+		
+		FibonacciSeries fObj = new FibonacciSeries();
+		fObj.Fibonacci(input);
+		scanner.close();
 	}
-
+	
+	void Fibonacci(int number) {
+		int numOne = 0, numTwo = 1;
+		int counter = 0;
+		System.out.print(numOne + " " + numTwo + " ");
+		
+		for (int i = 1; i <= number; i++) {
+			counter = numOne + numTwo;
+			System.out.print(counter + " ");
+			
+			numOne = numTwo;
+			numTwo = counter;
+		}
+	}
 }
